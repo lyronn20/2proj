fcb7feb72467498e3d4acdd31d4357be8178b827
lyronn <lyronn2012@gmail.com>
2025-05-13 22:24:03 +0200

    stats

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,16 +2,19 @@ extends Node2D
 
 @onready var grille_logique = $GrilleLogique
 @onready var map = $Map
-@onready var menu = $CanvasLayer/Menu  # On accède au Menu, qui s'occupe du label
+@onready var menu = $CanvasLayer/Menu
+@onready var stats = $CanvasLayer/Menu/HUD/Infos_Stats
 
 var last_cell = null
 
 func _ready():
 	map.set_process_unhandled_input(true)
-	
 
 func _process(_delta):
 	var cell = grille_logique.local_to_map(get_global_mouse_position())
 	if cell != last_cell:
 		last_cell = cell
 		menu.set_mouse_coords(cell)
+
+		# ⬇️ Simulation de mise à jour des stats
+		stats.update_stats(42, Vector2i(45, 50), Vector2i(38, 40), 22)
---
+++ scripts/infos_stats.gd
@@ -0,0 +1,35 @@
+extends Panel
+
+@onready var lbl_population = $VBoxContainer/Population
+@onready var lbl_housing = $VBoxContainer/Housing
+@onready var lbl_jobs = $VBoxContainer/Jobs
+@onready var lbl_progress = $VBoxContainer/HBoxContainer/Progress
+@onready var bar_progress = $VBoxContainer/HBoxContainer/ProgressBar
+@onready var lbl_timer = $VBoxContainer/Timer
+@onready var lbl_ticks = $VBoxContainer/Ticks_sec
+
+var time_elapsed := 0.0
+var tick_speed := 1
+
+func _ready():
+	var color = Color.html("#e9bc96")  # couleur demandée
+	lbl_population.add_theme_color_override("font_color", color)
+	lbl_housing.add_theme_color_override("font_color", color)
+	lbl_jobs.add_theme_color_override("font_color", color)
+	lbl_progress.add_theme_color_override("font_color", color)
+	lbl_timer.add_theme_color_override("font_color", color)
+	lbl_ticks.add_theme_color_override("font_color", color)
+
+func _process(delta):
+	time_elapsed += delta
+	var mins = int(time_elapsed / 60)
+	var secs = int(time_elapsed) % 60
+	lbl_timer.text = "⏱ %02d:%02d" % [mins, secs]
+	lbl_ticks.text = "%d ticks/sec" % int(60 * tick_speed)
+
+func update_stats(pop: int, housing: Vector2i, jobs: Vector2i, progress: int):
+	lbl_population.text = "👥 Population : %d" % pop
+	lbl_housing.text = "🏠 Housing : %d / %d" % [housing.x, housing.y]
+	lbl_jobs.text = "🛠 Jobs : %d / %d" % [jobs.x, jobs.y]
+	lbl_progress.text = "Progress : %d%%" % progress
+	bar_progress.value = progress
