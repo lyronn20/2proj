d9424838fefc08412179c006e628c9f120436c03
lyronn <lyronn2012@gmail.com>
2025-05-19 15:00:28 +0200

    popup bat

--- scripts/TableauBord.gd
+++ scripts/TableauBord.gd
@@ -1,28 +1,35 @@
 extends Control
 
-@onready var vbox := $VBoxContainer
+@onready var batiment_panel := $Batiment
+@onready var vbox := $Batiment/VBoxContainer
 
 func _ready():
 	await get_tree().process_frame
+	batiment_panel.visible = false  # caché au démarrage
 	update_dashboard()
 
-
 func update_dashboard(batiment: Node = null):
+	batiment_panel.visible = false
 	for child in vbox.get_children():
 		child.queue_free()
 
 	if batiment:
+		batiment_panel.visible = true
+
 		var label = Label.new()
 		label.text = "🏠 " + batiment.name
+		label.add_theme_color_override("font_color", Color("#e9bc96"))
 		vbox.add_child(label)
 
 		if "habitants" in batiment:
 			for p in batiment.habitants:
 				var p_label = Label.new()
 				p_label.text = "  → PNJ ID: " + str(p.id)
+				p_label.add_theme_color_override("font_color", Color("#e9bc96"))
 				vbox.add_child(p_label)
 		elif "employes" in batiment:
 			for p in batiment.employes:
 				var p_label = Label.new()
 				p_label.text = "  → PNJ ID: " + str(p.id)
+				p_label.add_theme_color_override("font_color", Color("#e9bc96"))
 				vbox.add_child(p_label)
